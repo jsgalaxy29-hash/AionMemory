4edbf16ecaed9962e4175f8b27be03471a8bb208
jsgalaxy29-hash <jsgalaxy29@gmail.com>
2025-11-23 20:55:51 +0100

    0.0.6

--- Aion.Infrastructure/obj/Debug/net10.0/Aion.Infrastructure.AssemblyInfo.cs
+++ Aion.Infrastructure/obj/Debug/net10.0/Aion.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aion.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab110b66e72e9c54082e8d6d9c7dbcd2900aeed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1820435ae46b252b614a3cce776998613dab9348")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aion.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aion.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
